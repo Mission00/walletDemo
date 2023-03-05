b212f804c323c16e04ca6e5a55466bf74266ce8c
cmx <1170485357@qq.com>
2023-03-05 13:47:18 +0800

    建表语句

--- src/main/resources/SQL.sql
+++ src/main/resources/SQL.sql
@@ -6,7 +6,7 @@ create table consume_detail
     `wallet_id`   int      not null comment '钱包ID',
     `order_id`    int comment '订单ID',
     `create_time` datetime null comment '创建时间',
-    `amount`      decimal  not null comment '消费金额',
+    `amount`      decimal(19,4)  not null comment '消费金额',
     `type`        tinyint  not null comment '明细类型：0：充值 1：消费 3：退款',
     `remark`      varchar(125) comment '备注'
 );
@@ -18,7 +18,7 @@ create table wallet
         primary key,
     `user_id`     int               not null comment '用户ID',
     `name`        varchar(10)       not null comment '钱包名',
-    `balance`     decimal default 0 not null comment '钱包余额',
+    `balance`     decimal(19,4) default 0 not null comment '钱包余额',
     `create_time` datetime          not null comment '钱包创建时间',
     `modify_time` datetime          not null comment '钱包修改时间',
     `version`     int     default 0 not null comment '版本号'
